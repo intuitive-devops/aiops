5c0546a7cf81b05306f34ba310e7fffb54e3ee67
cartheur <cartheur@pm.me>
2024-07-03 16:16:36 +0200

    Add wait event

--- network-man/src/run/Program.cs
+++ network-man/src/run/Program.cs
@@ -27,6 +27,7 @@ namespace run
             Console.WriteLine(" CIDR: " + RangeB.ToCidrString()); // is 192.168.0.0/24
 
             Console.WriteLine("Probe completed.");
+            Console.Readline();
         }
     }
 }
