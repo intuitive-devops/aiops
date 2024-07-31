dc20e1d4aed0301b493b15f3d894c77e1bf39b4e
cartheur <cartheur@pm.me>
2024-07-31 12:32:55 +0200

    Add practical data

--- network-man/src/run/Program.cs
+++ network-man/src/run/Program.cs
@@ -7,28 +7,30 @@ namespace run
 {
     internal class Program
     {
-        static IPAddressRange RangeA { get;  set; }
-        static IPAddressRange RangeB { get;  set; }
+
+        static IPAddressRange RangeA { get; set; }
+        static IPAddressRange RangeB { get; set; }
 
         static void Main(string[] args)
         {
+
+
             // rangeA.Begin is "192.168.0.0", and rangeA.End is "192.168.0.255".
             RangeA = new IPAddressRange();
-            var rangeA = IPAddressRange.Parse("192.168.0.0/255.255.255.0");
+            var rangeA = IPAddressRange.Parse("172.22.0.0/18");
             Console.WriteLine("Parse: " + rangeA);
-            Console.WriteLine(" Range: " + RangeA.Contains(IPAddress.Parse("192.168.0.34"))); // is True.
-            Console.WriteLine(" Range not: " + RangeA.Contains(IPAddress.Parse("192.168.10.1"))); // is False.
+            Console.WriteLine(" Range: " + RangeA.Contains(IPAddress.Parse("172.28.0.0"))); // is True.
+            Console.WriteLine(" Range not: " + RangeA.Contains(IPAddress.Parse("172.22.62.1"))); // is False.
             Console.WriteLine(" CIDR: " + RangeA.ToCidrString()); // is 192.168.0.0/24
             RangeB = new IPAddressRange();
             // rangeB.Begin is "192.168.0.10", and rangeB.End is "192.168.10.20".
-            var rangeB1 = IPAddressRange.Parse("192.168.0.10 - 192.168.10.20");
+            var rangeB1 = IPAddressRange.Parse("172.28.88.0/24");
             Console.WriteLine("Parse: " + rangeB1);
-            Console.WriteLine(" Range: " + RangeB.Contains(IPAddress.Parse("192.168.3.45"))); // is True.
-            Console.WriteLine(" Range not: " + RangeB.Contains(IPAddress.Parse("192.168.0.9"))); // is False.
+            Console.WriteLine(" Range: " + RangeB.Contains(IPAddress.Parse("172.22.0.0"))); // is True.
+            Console.WriteLine(" Range not: " + RangeB.Contains(IPAddress.Parse("172.28.87.9"))); // is False.
             Console.WriteLine(" CIDR: " + RangeB.ToCidrString()); // is 192.168.0.0/24
 
             Console.WriteLine("Probe completed.");
             Console.ReadLine();
         }
     }
-}
